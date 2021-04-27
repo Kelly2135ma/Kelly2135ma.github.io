9ee6b8f647a77d11632a93ab6374c8fd81515e31
Kelly Ma <kelly2135ma@gmail.com>
2021-04-27 11:40:37 -0700

    added assets

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+function eatCake() {
+    document.getElementById("cakepicture").src = "assets/img/eatCake.png";
+}
+
+document.getElementById("cakeButton").onclick = eatCake;
+
+function confetti() {
+    document.getElementById("cakepicture").src = "assets/img/confettiCake.png";
+}
+
+document.getElementById("confettiButton").onclick = confetti;
+
+function heart() {
+    document.getElementById("cakepicture").src = "assets/img/heartCake.png";
+}
+
+document.getElementById("heartButton").onclick = heart;
